a1304fa0132c8d87115b440450af01447f6a0cd9
vouzze <60976722+vouzze@users.noreply.github.com>
2020-02-15 14:27:54 +0200

    Update Test1.cs

--- UnitTestProject1/Test1.cs
+++ UnitTestProject1/Test1.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Test
+namespace UnitTest1
 {
     [TestClass]
-    public class LABA1test
+    public class Test1
     {
         [TestMethod]
         public void TestMethod1()
         {
             int a = 3;
             int b = 10;
-            double result = LAB1.Program.Square(a, b);
+            double result = ConsoleApp1.Program.Square(a, b);
             Assert.AreEqual(188.4, result);
         }
     }
